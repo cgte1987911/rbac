7a160969e530e5d6a472f0579e243ec11f7f72c5
cgte1987911 <hongfa2011@126.com>
2018-11-12 12:11:51 +0800

    eclipse热加载

--- rabc-purview/src/main/java/com/ltf/controllers/SampleController.java
+++ rabc-purview/src/main/java/com/ltf/controllers/SampleController.java
@@ -1,22 +1,28 @@
 package com.ltf.controllers;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import com.ltf.utils.JsonData;
 
 import com.ltf.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
-    @RequestMapping("/hello")
+    @RequestMapping("/hello2")
 
     public String  home(){
 
         return "test";
+    }
+    @RequestMapping(value = "/echo/{message}",method = RequestMethod.GET)
+    @ResponseBody
+    public String  echo(@PathVariable("message") String msg){
+
+        return "aaaaf123"+msg;
     }
 
     @RequestMapping(value = "/testfd" ,method=RequestMethod.POST)
