b46839d53ccc8cd5ab8409366d544b7c5999f1fb
hongf <hongf@DESKTOP-LIF9QJU>
2018-11-13 10:16:22 +0800

    checkstyle格式化代码

--- rabc-purview/src/main/java/com/ltf/configs/WebMvcConfig.java
+++ rabc-purview/src/main/java/com/ltf/configs/WebMvcConfig.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
 
 /**
  * @author dreamneverdie
